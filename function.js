018d35560d5629c5aceeaae78dbcb74fd773ee0d
System admin <admin1@rahyaft.local>
2022-05-26 16:07:08 +0430

    error in js code not solved

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
 
+
 let counter_element = document.body.querySelector("transact-col span");
 let counter_value = counter_element.textContent;
 counter_element.textContent = Number(counter_value) + 1;
