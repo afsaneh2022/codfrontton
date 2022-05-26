f6d1c6ff0c9aefb3e64d684bcf2db430e05c5cda
System admin <admin1@rahyaft.local>
2022-05-26 15:22:18 +0430

    js function

---
+++ function.js
@@ -0,0 +1,5 @@
+
+let counter_element = document.body.querySelector("transact-col span");
+let counter_value = counter_element.textContent;
+counter_element.textContent = Number(counter_value) + 1;
+console.log(counter_element);
