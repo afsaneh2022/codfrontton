b042ee6d31f4e86a13717b7f8316304b7423655e
System admin <admin1@rahyaft.local>
2022-05-28 13:59:36 +0430

    change transact counter

--- function.js
+++ function.js
@@ -1,6 +1,19 @@
 
 
-let counter_element = document.body.querySelector("transact-col span");
-let counter_value = counter_element.textContent;
-counter_element.textContent = Number(counter_value) + 1;
-console.log(counter_element);
+let counter = document.body.querySelector(".test1");
+console.log(counter.textContent)
+
+function get(){
+    current=counter.textContent;
+    current=Number(current);
+    return current;
+}
+console.log(get());
+
+function increement(){
+    let current=get();
+    let next=current + 1;
+    counter.textContent=next;
+}
+
+setInterval(increement,1000)  ;
