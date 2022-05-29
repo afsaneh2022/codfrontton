a76ef602e352bf53f4af6c7c7083ddf0e724c4fd
afsaneh2022 <afsaneh.jafarigm@gmail.com>
2022-05-29 12:55:59 +0430

    change function counter

--- function.js
+++ function.js
@@ -1,17 +1,15 @@
 
-
-let counter = document.body.querySelector(".transact-count");
+//    get grom html code 
+ let counter = document.body.querySelector(".transact-count");
 
 function get(){
     current=counter.textContent;
     current=Number(current);
     return current;
 }
-
+let current=58281750 
 function increement(){
-    let current=get();
-    let next=current + 1;
-    counter.textContent=next;
+    current++ ;
+    counter.textContent=current;
 }
-
 setInterval(increement,1000)  ;
